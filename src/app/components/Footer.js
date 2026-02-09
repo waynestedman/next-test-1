1d6e3c1cdd43b15c1b423387f86fdc4c38207f09
Wayne Stedman <waynestedman@users.noreply.github.com>
2026-02-09 14:22:28 -0800

    add footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,16 @@
+// site footer
+
+import styles from './footer.module.css';
+
+function Footer() {
+
+  return (
+    <footer className={styles.footer}>
+      <p>Not a company</p>
+      <p>Made by Someone, somewhere</p>
+      <p>Copyright 2026</p>
+    </footer>
+  )
+}
+
+export default Footer;
