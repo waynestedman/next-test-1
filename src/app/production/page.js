2955af1e2adbc622a46ff5de6f020b829570ad6e
Wayne Stedman <waynestedman@users.noreply.github.com>
2026-02-10 15:41:17 -0800

    checkboxes for send to prod

--- src/app/production/page.js
+++ src/app/production/page.js
@@ -8,8 +8,16 @@ function Production() {
 
   return (
     <div className={"page " + styles.productionPage}>
-      <h3>Production</h3>
-      <p>Click to send these models into production.</p>
+      <h3>Send to Production</h3>
+      <div className={styles.selectionList}>
+        <h5>Selection which models you would like to send into production.</h5>
+        <div className={styles.checkboxList}>
+          <label><input type="checkbox" name="models" value="bolt" /> Bolt</label>
+          <label><input type="checkbox" name="models" value="housing" /> Housing</label>
+          <label><input type="checkbox" name="models" value="gear" /> Gear</label>
+          <label><input type="checkbox" name="models" value="assembly" /> Assembly</label>
+        </div>
+      </div>
       <WButton 
         label="Send to Production"
         onClick={() => alert("Model(s) sent to production!")}></WButton>
